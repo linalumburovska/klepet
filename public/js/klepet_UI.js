939d5a1b787790e650cfd4505392c9fd02e773f7
Lina Lumburovska <linalumburovska20@gmail.com>
2016-04-09 16:47:03 +0000

    Zaključek dela na IV. nalogi

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -102,7 +102,7 @@ $(document).ready(function() {
   });
   
   socket.on('dregljaj', function() {
-    $('#vsebina').jrumble();
+    $('#vsebina').jrumble() ;
     $('#vsebina').trigger('startRumble');
     setTimeout(function() {
       $('#vsebina').trigger('stopRumble');
