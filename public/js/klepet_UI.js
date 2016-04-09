6276ec3588d13169b8f05a20fcf682822b593ba7
Lina Lumburovska <linalumburovska20@gmail.com>
2016-04-09 16:57:28 +0000

    Mala promena

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -6,6 +6,7 @@ function divElementEnostavniTekst(sporocilo) {
 }
 
 function divElementHtmlTekst(sporocilo) {
+  sporocilo = sporocilo.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace('&lt;img', '<img').replace('png\' /&gt;', 'png\' />');
    sporocilo = dodajSlika(sporocilo);
   return $('<div></div>').html('<i>' + sporocilo + '</i>');
 }
